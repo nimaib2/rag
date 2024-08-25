e6692b2e6335bd240b9fbfec63dc9b1cddf097f0
nimai-belur <nimaibelur@outlook.com>
2024-08-25 12:12:39 -0700

    Created the main page, added chatGPT implementation. Fixed issues with sendMessage

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -27,6 +27,7 @@ Ready to assist the student in finding the perfect professor based on their need
 `
 
 export async function POST(req){
+    console.log('POST method running')
     const data = await req.json()
 
     const pc = new Pinecone({
@@ -34,10 +35,10 @@ export async function POST(req){
     })
 
     const index = pc.index('rag').namespace('ns1')
-    const openai=new OpenAI()
+    const openai=new OpenAI({apiKey: process.env.OPEN_AI_API_KEY})
 
     const text = data[data.length-1].content
-    const embedding = await OpenAI.Embeddings.create({
+    const embedding = await openai.embeddings.create({
         model: 'text-embedding-3-small',
         input: text,
         encoding_format: 'float',
@@ -49,14 +50,51 @@ export async function POST(req){
         vector: embedding.data[0].embedding
     })
 
-    let resultString = 'Returned results: '
+    let resultString = '\n\nReturned results from vector db (done automatically)'
     results.matches.forEach((match)=>{
         resultString+=`\n
         Professor: ${match.id}
         Review: ${match.metadata.stars}
-        Subject: ${match.metadata}
-        Stars:
+        Subject: ${match.metadata.subject}
+        Stars: ${match.metadata.stars}
         \n\n
         `
     })
+
+    const lastMessage = data[data.length-1]
+    const lastMessageContent=lastMessage.content + resultString
+    const lastDataWithoutLastMessage=data.slice(0, data.length-1)
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {role: 'system', content: systemPrompt},
+            ...lastDataWithoutLastMessage,
+            {role: 'user', content: lastMessageContent}
+        ],
+        model: 'gpt-4o-mini',
+        stream: true
+        
+    })
+
+    const stream = new ReadableStream({
+        async start(controller){
+            const encoder = new TextEncoder()
+            //encodes each chunk of the text, adding it to the stream
+            try{
+                for await (const chunk of completion){
+                    const content = chunk.choices[0]?.delta?.content
+                    if(content){
+                        const text=encoder.encode(content)
+                        controller.enqueue(text)
+                    }
+                    
+                }
+            } catch (err) {
+                controller.error(err)
+            } finally {
+                controller.close()
+            }
+        }
+    })
+
+    return new NextResponse(stream)
 }
